d4267e455278b20110413ce5332e68e07e9b27aa
msizi-mlaba <msizismlaba@gmail.com>
2026-08-30 10:47:59 -0400

    isLoggedIn and userMessage code snippet update

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -1,4 +1,4 @@
-
+/* Define variables and if else statement for userRole and accessLevel  */
 let userRole = "admin";
 let accessLevel;
 
@@ -12,4 +12,17 @@ if (userRole === "admin") {
 
 console.log("Access Level:", accessLevel);
 
+/* Define variables and nested if else statements for isLoggedIn and userMessage */
+let isLoggedIn = true;
+let userMessage;
 
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!"   
+    } else {
+        userMessage = "Welcome, User!";
+    }
+
+} else {
+    userMessage = "Please log in to access the system.";
+}
