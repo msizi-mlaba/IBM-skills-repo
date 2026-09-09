202780af611b281ed2ceab08a416daac8491cf11
msizi-mlaba <msizismlaba@gmail.com>
2026-09-09 15:26:50 +0200

    reset counter button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,8 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+
+function resetCount() {
+    count = 0; // Reset count to 0
+    displayCount(); // Display the reset count
+}
