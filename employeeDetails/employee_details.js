23644fa214f877879c82ba209c9cce1e51e52197
msizi-mlaba <msizismlaba@gmail.com>
2026-09-14 16:55:51 +0200

    Specialization JavaScript Update

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,20 +1,20 @@
 // employee database array of objects
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Recruitment' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Accounting' },
       //... More employee records can be added here
     ];
 
 // create a function to get employee details
 function displayEmployees() {
     const totalEmployees = employees.map(employee => `<p>${employee.name}: ${employee.department} - ${employee.salary}</p>`).join('');
-    document.getElementById('employeeDetails').innerHTML = totalEmployees;
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
 
 // calculate the salary of employees
 function calculateTotalSalaries(){
-    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    const totalSalary = employees.reduce((acc, employee) => acc + employee.salary, 0);
     alert(`Total Salary: ${totalSalary}`);
 }
 
@@ -31,6 +31,16 @@ function findEmployeeById(employeeId) {
       }
       else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+       }
+   }
+
+function findEmployeeBySpecialization(employeeSpecialization) {
+      const foundEmployee = employees.find(employee => employee.specialization === employeeSpecialization);
+      if (foundEmployee) {
+      document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.department} - ${foundEmployee.salary} - ${foundEmployee.specialization} </p>`;
+      }
+      else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
        }
    }
 
